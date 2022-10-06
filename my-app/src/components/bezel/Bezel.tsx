63eec9ba6c9a9ac27613ea2f49fd3c09c6fbd67b
GabrielGladykowski <gabriel.gladki2003@gmail.com>
2022-10-06 14:17:16 +0200

    little mistake

--- my-app/src/components/bezel/Bezel.tsx
+++ my-app/src/components/bezel/Bezel.tsx
@@ -3,12 +3,11 @@ import { Dial } from "../dial/Dial";
 import { BezelStyled } from "./Bezel.styled";
 import { Decoration } from "./decoration/Decoration";
 import { ARRAY_FROM_DECORATION } from "./decoration/Decoration.data";
-import { DecorationStyled, DecorationWrapperStyled } from "./decoration/Decoration.styled";
 
 export const Bezel = () => (
     <BezelStyled>
         <Dial />
-        
+
         {ARRAY_FROM_DECORATION.map((props, index) => (
             <Decoration key={index} {...props}/>
         ))}
