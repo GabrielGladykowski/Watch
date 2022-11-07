1af81ecf7b643e68cef623e353e341724ce749f1
GabrielGladykowski <gabriel.gladki2003@gmail.com>
2022-11-07 22:45:26 +0100

    Create Hands logic

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import GLOBALSTYLE from './global/GlobalStyle';
 import { Watch } from './components/Watch';
+import { HandsContext, HandsProvider } from './components/dial/hands/HandsContext';
 
 function App() {
   return (
-    <>
+    <HandsProvider>
       <GLOBALSTYLE />
       <Watch />
-    </>
+    </HandsProvider>
     );
 }
 
